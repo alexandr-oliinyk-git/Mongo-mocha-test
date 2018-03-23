b8cfa127c1f60b59e0de0bc39502ef8bd88fc7c0
alexandr-oliinyk-git <alexandr.oliinyk@gmail.com>
2018-03-23 11:52:22 +0200

    add update

--- test/delete_test.js
+++ test/delete_test.js
@@ -4,37 +4,45 @@ const User = require('../src/user');
 describe('deleting a user', () =>{
     let joe;
   
-
     beforeEach(done => {
         joe = new User({ name: 'Joe' });
         joe.save()
-            .then(done());
+            .then(() => done());
     });
 
     it('model instance remove', done => {
-        this.timeout(10000);
         joe.remove()
-            .then(User.findOne({ name: 'Joe' }))
+            .then(() => User.findOne({ name: 'Joe' }))
             .then(user => {
                 assert(user === null);
                 done();
             });
     });
 
-    // it('class method remove', done => {
-    //     User.remove({ name: 'Joe' })
-    //         .then(User.findOne({ name: 'Joe' }))
-    //         .then(user => {
-    //             assert(user === null);
-    //             done();
-    //         });
-    // });
+    it('class method remove', done => {
+        User.remove({ name: 'Joe' })
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then(user => {
+                assert(user === null);
+                done();
+            });
+    });
 
-    // it('class method findAndRemove',() => {
+    it('class method findOneAndRemove', done => {
+        User.findOneAndRemove({ name: 'Joe'})
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then(user => {
+                assert(user === null);
+                done();
+            });
+    });
 
-    // });
-
-    // it('class method findById',() => {
-
-    // });
+    it('class method findById', done => {
+        User.findByIdAndRemove(joe._id)
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then(user => {
+                assert(user === null);
+                done();
+            });
+    });
 });
