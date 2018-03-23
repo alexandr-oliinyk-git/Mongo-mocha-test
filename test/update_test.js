34e659e08546696a343aab346e8ff65aefe86e88
alexandr-oliinyk-git <alexandr.oliinyk@gmail.com>
2018-03-23 15:17:44 +0200

    add validation

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', done => {
     let joe;
 
     beforeEach(done =>{
-        joe = new User({ name: 'Joe' });
+        joe = new User({ name: 'Joe' , postCount: 0});
         joe.save()
             .then(() => done());
     });
@@ -39,5 +39,15 @@ describe('Updating records', done => {
     
     it('a model class can find a record with Id & update', done => {
         assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
+    });
+
+    it('User can have their postcount increment by 1', done => {
+        User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then(user => {
+                assert(user.postCount === 1);
+                done();
+            })
+
     });
 });
